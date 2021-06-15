03836c3d3f7707a92231fb489ae30d65b898fa18
Ben <schackmanb24@greenhill.org>
2021-06-15 00:39:18 -0500

    CHALLENGE HW 6/14/2021

---
+++ ArjunTeachingBen/whileLoops.py
@@ -0,0 +1,24 @@
+#variables: Something that holds a value
+#   they can be changed, set, or used to set other variables
+blab = 0
+blab = 3
+#loop: it continually runs code until told to stop
+#2 types of loops: for loops and while loops
+
+#while [condition]
+blab = 3
+# while blab>0:
+#     print(blab)
+#     blab = blab-1
+blab = 0
+while blab<=10:
+    print(blab)
+    blab= blab +1
+
+
+# 0
+# 2
+# 4
+# 6
+# 8
+# 10
